2a87db8a7585c04f1b1b4c126eb8aa17986c1a18
Joe Saraceno <honestcomrade@gmail.com>
2017-10-30 21:10:08 -0700

    checking formatting

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,3 @@ ADD . /code
 WORKDIR /code
 RUN pip install -r requirements.txt
 CMD ["python", "app.py"]
-
--- app.py
+++ app.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
-
 def hello():
-  count=redis.incr('hits')
-  return 'Hello World! I have been seen {} times.\n'.format(count)
+    count = redis.incr('hits')
+    return 'Hello World! I have been seen {} times.\n'.format(count)
 
-  if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
